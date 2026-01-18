4fbc5efc0cc6bfa68cbdbc2a5d0b80353286a18d
Phạm Thanh Huy <huyp7922@gmail.com>
2026-01-18 19:44:25 +0700

    feat: implement initial database and queue
    - Create `users` and `posts` tables
    - Add token revocation tables
    - Define `queues` table with default status
    - Integrate queue module into

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const router = require("./src/routes");
 const apiRateLimiter = require("./src/middlewares/rateLimiter");
 const exceptionHandler = require("./src/middlewares/exceptionHandler");
 const taskMap = require("./src/tasks");
-
+require("./queue");
 const app = express();
 
 app.use(express.json());
